a553aadd6a9be70c125b33c9121117f7b9b5e5e5
henry.shen <henry.shen@onelab.tw>
2022-10-03 14:50:50 +0800

    update health check

--- copy-ca-files.sh
+++
@@ -1,3 +0,0 @@
-docker cp elk-es01:/usr/share/elasticsearch/config/certificates/ca/ca.crt ./elasticsearch/certs/ca.crt
-docker cp elk-es01:/usr/share/elasticsearch/config/certificates/es01/es01.crt ./elasticsearch/certs/es01.crt
-docker cp elk-es01:/usr/share/elasticsearch/config/certificates/es01/es01.key ./elasticsearch/certs/es01.key
